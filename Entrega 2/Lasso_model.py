4fbbb8054454f54364c2bdfb9a30db7da4209114
Lizeth Garcia <lj.garciaa1@uniandes.edu.co>
2025-11-08 21:23:41 -0500

    Agregar modelo Lasso

---
+++ Entrega 2/Lasso_model.py
@@ -0,0 +1,78 @@
+# ==============================================================
+# ENTRENAMIENTO DE MODELOS - PROYECTO DESPLIEGUE DE SOLUCIONES ANALÍTICAS
+# Predicción del precio de viviendas en EE. UU.
+# ==============================================================
+
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import itertools
+
+# ==============================================================
+# 1. CONEXIÓN A MLFLOW REMOTO (EC2)
+# ==============================================================
+mlflow.set_tracking_uri("http://3.214.199.14:5000")
+mlflow.set_experiment("Linear Regression - Lasso")
+
+# ==============================================================
+# 2. CARGA Y PREPROCESAMIENTO DE DATOS
+# ==============================================================
+df = pd.read_csv("data/USAHousingDataset.csv")
+
+# Limpieza básica
+df = df.drop(columns=['date', 'street', 'statezip', 'country'])
+df = df[df['price'] > 0]
+df = pd.get_dummies(df, columns=['city'])
+
+# Variables predictoras y objetivo (precio en log)
+X = df.drop(columns=['price'])
+y = np.log(df['price'])
+
+# División de datos
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# ==============================================================
+# 3. PARAMETROS
+# ==============================================================
+param_grid = {
+    "alpha": [0.1, 1.0, 10.0]   
+}
+
+param_combinations = list(itertools.product(
+    param_grid["alpha"],
+))
+
+# ==============================================================
+# 4. LOOP DE ENTRENAMIENTO Y REGISTRO EN MLFLOW
+# ==============================================================
+for (alpha,) in param_combinations:
+
+    nombre = f"Lasso_alpha_{alpha}".replace(".", "_")
+    
+    with mlflow.start_run(run_name=nombre):
+        #Entrenamiento del modelo
+        modelo = Lasso(alpha=alpha, max_iter=10000, random_state=0)
+        modelo.fit(X_train, y_train)
+        y_pred = modelo.predict(X_test)
+
+        # Métricas de evaluación
+        mae = mean_absolute_error(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        rmse = np.sqrt(mse)
+        r2 = r2_score(y_test, y_pred)
+
+        # Registro en MLflow
+        mlflow.log_param("Modelo", nombre)
+        mlflow.log_metric("MAE", mae)
+        mlflow.log_metric("MSE", mse)
+        mlflow.log_metric("RMSE", rmse)
+        mlflow.log_metric("R2", r2)
+        mlflow.sklearn.log_model(modelo, name=nombre)
+
+        print(f"{nombre} → RMSE: {rmse:.3f} | R²: {r2:.3f}")
+
+print("\n Todos los modelos fueron registrados correctamente en MLflow remoto (EC2).")
